3a65d50d1b4f52a29677159e61bf4fb034612837
angondim <andregondim0@gmail.com>
2024-06-05 21:20:10 -0300

    Projeto Concluído

---
+++ src/controller/Emissao.java
@@ -0,0 +1,14 @@
+package controller;
+
+import model.Contracheque;
+
+public class Emissao {
+
+	public static void main(String[] args) {
+		Contracheque contracheque = Contracheque.questionario();
+		contracheque.relatorio();
+		
+
+	}
+
+}
